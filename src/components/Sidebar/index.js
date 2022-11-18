278836b958f15bb9cf46904aec28ae7cb32de597
CatW-M <wheelermushlin@gmail.com>
2022-11-18 13:29:34 -0700

    set up and sidebar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,77 @@
+import { Link, NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import './index.scss';
+import Logo from '../../assets/images/Logo1.png';
+import LogoSubTitle from '../../assets/images/Sublogo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faLinkedin,
+    faGithub,
+} from '@fortawesome/free-brands-svg-icons';
+import { 
+    faEnvelope, 
+    faHome, 
+    faUser,
+    faBars,
+    faClose,
+} from '@fortawesome/free-solid-svg-icons';
+
+const Sidebar = () => {
+    const [showNav, setShowNav] = useState(false);
+
+    return (
+    <div className='nav-bar'>
+        <Link className='logo' to='/' onClick={() => setShowNav(false)}>
+            <img src={Logo} alt="CWM Logo" />
+            <img className="sub-logo" src={LogoSubTitle} alt="CWM Logo" />
+        </Link>
+        <nav className={showNav ? 'mobile-show' : '' }>
+            <NavLink exact="true" activeclassname="active" to="/" onClick={() => setShowNav(false)}>
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about" onClick={() => setShowNav(false)}>
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact" onClick={() => setShowNav(false)}>
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+            <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faClose} color="#ffd700" size='3x' className='close-icon' />
+        </nav>
+        <ul>
+            <li>
+            <a
+            href="https://www.linkedin.com/in/compsciwheeler"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+            </li>
+            <li>
+          <a
+            href="https://github.com/CatW-M"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        </ul>
+        <FontAwesomeIcon 
+          onClick={() => setShowNav(true)}
+          icon={faBars}
+          color="#ffd700"
+          size="3x"
+          className='hamburger-icon' />
+    </div>
+)
+}
+export default Sidebar;
