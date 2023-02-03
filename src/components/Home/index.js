45ae92f86febdd96d45121767f88e8906e4c6e7f
CatW-M <wheelermushlin@gmail.com>
2023-02-03 13:51:12 -0700

    removed duplicate code

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
-import './index.scss';
+
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
