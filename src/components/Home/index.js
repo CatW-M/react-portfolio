a51d054b41c291e8332fceaad497c0415174c78b
Catherine Wheeler-Mushlin <107967088+CatW-M@users.noreply.github.com>
2023-03-10 22:59:20 -0700

    styling home page and nav bar light

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,37 +1,69 @@
 import './index.scss';
-import LogoTitle from '../../assets/images/Logo1.png';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
-  const [letterClass, setLetterClass] = useState('text-animate')
+  const [letterClass, setLetterClass] = useState('text-animate');
 
-  const firstNameArray = ['C', 'a', 't', 'h', 'e', 'r', 'i', 'n', 'e']
-  const lastNameArray = ['W', 'h', 'e', 'e', 'l', 'e', 'r', '-', 'M', 'u', 's', 'h', 'l', 'i', 'n', ',']
-  const jobArray = ['S', 'o', 'f', 't', 'w', 'a', 'r', 'e', '', 'E', 'n', 'g', 'i', 'n', 'e', 'e', 'r']
+  const firstNameArray = ['C', 'a', 't', 'h', 'e', 'r', 'i', 'n', 'e'];
+  const lastNameArray = [
+    'W',
+    'h',
+    'e',
+    'e',
+    'l',
+    'e',
+    'r',
+    '-',
+    'M',
+    'u',
+    's',
+    'h',
+    'l',
+    'i',
+    'n',
+    ',',
+  ];
+  const jobArray = [
+    'S',
+    'o',
+    'f',
+    't',
+    'w',
+    'a',
+    'r',
+    'e',
+    '',
+    'E',
+    'n',
+    'g',
+    'i',
+    'n',
+    'e',
+    'e',
+    'r',
+  ];
 
   useEffect(() => {
     setTimeout(() => {
-      setLetterClass('text-animate-hover')
-    }, 4000)
-  }, [])
+      setLetterClass('text-animate-hover');
+    }, 4000);
+  }, []);
 
   return (
     <>
       <div className="container home-page">
         <div className="text-zone">
           <h1>
-            <span className={letterClass}>H</span>
+            {/* <span className={letterClass}>H</span>
             <span className={`${letterClass} _12`}>i,</span>
             <br />
             <span className={`${letterClass} _13`}>I</span>
-            <span className={`${letterClass} _14`}>'m</span>
-            <img
-              src={LogoTitle}
-              alt="JavaScript Developer Name, Web Developer Name"
-            />
+            <span className={`${letterClass} _14`}>'m </span>
+            <span className={`${letterClass} _15`}> </span> */}
+
             <AnimatedLetters
               letterClass={letterClass}
               strArray={firstNameArray}
@@ -60,7 +92,7 @@ const Home = () => {
 
       <Loader type="pacman" />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,10 @@
 import { Link, NavLink } from 'react-router-dom';
 import { useState } from 'react';
 import './index.scss';
-import Logo from '../../assets/images/Logo1.png';
-import LogoSubTitle from '../../assets/images/Sublogo.png';
+import Logo from '../../assets/images/logo.png';
 import Resume from '../../assets/images/CwmResume010523.pdf';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faLinkedin,
-  faGithub,
-} from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import {
   faEnvelope,
   faHome,
@@ -17,75 +13,133 @@ import {
   faBars,
   faClose,
   faCode,
-  faDownload
+  faDownload,
 } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => {
   const [showNav, setShowNav] = useState(false);
 
   return (
-    <div className='nav-bar'>
-      <Link className='logo' to='/' onClick={() => setShowNav(false)}>
-        <img src={Logo} alt="CWM Logo" />
-        <img className="sub-logo" src={LogoSubTitle} alt="CWM Logo" />
-      </Link>
-      <nav className={showNav ? 'mobile-show' : ''}>
-        <NavLink end activeclassname="active" to="/" onClick={() => setShowNav(false)}>
-          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-        </NavLink>
-        <NavLink end activeclassname="active" className="about-link" to="/about" onClick={() => setShowNav(false)}>
-          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-        </NavLink>
-        <NavLink end activeclassname="active" className="portfolio-link" to="/portfolio" onClick={() => setShowNav(false)}>
-          <FontAwesomeIcon icon={faComputer} color="#4d4d4e" />
-        </NavLink>
-        <NavLink end activeclassname="active" className="skills-link" to="/skills" onClick={() => setShowNav(false)}>
-          <FontAwesomeIcon icon={faCode} color="#4d4d4e" />
-        </NavLink>
-        <NavLink end activeclassname="active" className="contact-link" to="/contact" onClick={() => setShowNav(false)}>
-          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-        </NavLink>
-        <a activeclassname="active" className="resume-link" href={Resume} download onClick={() => setShowNav(false)}>
-        <FontAwesomeIcon icon={faDownload} color="#4d4d4e" />
-        </a>
-        <FontAwesomeIcon onClick={() => setShowNav(false)} icon={faClose} color="#ffd700" size='3x' className='close-icon' />
-        
-      </nav>
-      <ul>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/compsciwheeler"
-            target="_blank"
-            rel="noreferrer"
-          >
+    <div className="nav-bar">
+      <div className="separate">
+        <Link className="logo" to="/" onClick={() => setShowNav(false)}>
+          <img src={Logo} alt="CWM Logo" />
+        </Link>
+      </div>
+      <div className="separate">
+        <nav className={showNav ? 'mobile-show' : ''}>
+          <div className="navLink">
+            <NavLink
+              end
+              activeclassname="active"
+              to="/"
+              onClick={() => setShowNav(false)}
+            >
+              <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+          </div>
+          <div className="navLink">
+            <NavLink
+              end
+              activeclassname="active"
+              className="about-link"
+              to="/about"
+              onClick={() => setShowNav(false)}
+            >
+              <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+          </div>
+          <div className="navLink">
+            <NavLink
+              end
+              activeclassname="active"
+              className="portfolio-link"
+              to="/portfolio"
+              onClick={() => setShowNav(false)}
+            >
+              <FontAwesomeIcon icon={faComputer} color="#4d4d4e" />
+            </NavLink>
+          </div>
+          <div className="navLink">
+            <NavLink
+              end
+              activeclassname="active"
+              className="skills-link"
+              to="/skills"
+              onClick={() => setShowNav(false)}
+            >
+              <FontAwesomeIcon icon={faCode} color="#4d4d4e" />
+            </NavLink>
+          </div>
+          <div className="navLink">
+            <NavLink
+              end
+              activeclassname="active"
+              className="contact-link"
+              to="/contact"
+              onClick={() => setShowNav(false)}
+            >
+              <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+          </div>
+          <div className="navLink">
+            <a
+              activeclassname="active"
+              className="resume-link"
+              href={Resume}
+              download
+              onClick={() => setShowNav(false)}
+            >
+              <FontAwesomeIcon icon={faDownload} color="#4d4d4e" />
+            </a>
             <FontAwesomeIcon
-              icon={faLinkedin}
-              color="#4d4d4e"
-              className="anchor-icon"
+              onClick={() => setShowNav(false)}
+              icon={faClose}
+              color="#ffd700"
+              size="3x"
+              className="close-icon"
             />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://github.com/CatW-M"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faGithub}
-              color="#4d4d4e"
-              className="anchor-icon"
-            />
-          </a>
-        </li>
-      </ul>
-      <FontAwesomeIcon
-        onClick={() => setShowNav(true)}
-        icon={faBars}
-        color="#ffd700"
-        size="3x"
-        className='hamburger-icon' />
+          </div>
+        </nav>
+      </div>
+      <div className="separate">
+        <ul>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/compsciwheeler"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                color="#4d4d4e"
+                className="anchor-icon"
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/CatW-M"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faGithub}
+                color="#4d4d4e"
+                className="anchor-icon"
+              />
+            </a>
+          </li>
+        </ul>
+        <FontAwesomeIcon
+          onClick={() => setShowNav(true)}
+          icon={faBars}
+          color="#001d46"
+          size="3x"
+          className="hamburger-icon"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 export default Sidebar;
